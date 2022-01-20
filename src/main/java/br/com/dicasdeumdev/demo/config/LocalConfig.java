d435b8d9a508d7429977cafb1410b8fa8a87f530
caiomelquiades <caio.mlima@protonmail.com>
2022-01-20 14:49:33 -0300

    Local configuration implemented. Object not found exception handling implemented

---
+++ src/main/java/br/com/dicasdeumdev/demo/config/LocalConfig.java
@@ -0,0 +1,26 @@
+package br.com.dicasdeumdev.demo.config;
+
+import br.com.dicasdeumdev.demo.domain.User;
+import br.com.dicasdeumdev.demo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Bean
+    public void startDB(){
+        User u1 = new User(null, "Valdir", "valdir@mail.com", "123");
+        User u2 = new User(null, "Luiz", "luiz@mail.com", "123");
+
+        userRepository.saveAll(List.of(u1, u2));
+    }
+}
