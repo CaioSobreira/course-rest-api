6f03c7313fc2594f11f44286ebc20daa19f0025c
caiomelquiades <caio.mlima@protonmail.com>
2022-01-20 11:28:14 -0300

    First commit

---
+++ src/test/java/br/com/dicasdeumdev/demo/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicasdeumdev.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
