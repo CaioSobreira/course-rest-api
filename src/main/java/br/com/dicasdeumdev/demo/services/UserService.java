ae79830584643eddbadb8c2f1f2f12b8cf5a6dfa
caiomelquiades <caio.mlima@protonmail.com>
2022-01-21 15:44:41 -0300

    User create endpoint implemented

--- src/main/java/br/com/dicasdeumdev/demo/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/demo/services/UserService.java
@@ -1,6 +1,7 @@
 package br.com.dicasdeumdev.demo.services;
 
 import br.com.dicasdeumdev.demo.domain.User;
+import br.com.dicasdeumdev.demo.domain.dto.UserDTO;
 
 import java.util.List;
 
@@ -8,4 +9,5 @@ public interface UserService {
 
     User findById(Integer id);
     List<User> findAll();
+    User create(UserDTO obj);
 }
