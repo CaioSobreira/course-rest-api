7c3575181a93393235da9c44e0c395a4e6eaf690
caiomelquiades <caio.mlima@protonmail.com>
2022-01-21 15:14:27 -0300

    User find all endpoint implemented

--- src/main/java/br/com/dicasdeumdev/demo/resources/UserResource.java
+++ src/main/java/br/com/dicasdeumdev/demo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package br.com.dicasdeumdev.demo.resources;
 
+import br.com.dicasdeumdev.demo.domain.User;
 import br.com.dicasdeumdev.demo.domain.dto.UserDTO;
 import br.com.dicasdeumdev.demo.services.UserService;
 import org.modelmapper.ModelMapper;
@@ -9,6 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -23,5 +27,11 @@ public class UserResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UserDTO> findById(@PathVariable Integer id){
         return ResponseEntity.ok().body(mapper.map(userService.findById(id), UserDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> findAll(){
+        return ResponseEntity.ok().body(userService.findAll()
+                        .stream().map(x -> mapper.map(x, UserDTO.class)).collect(Collectors.toList()));
     }
 }
--- src/main/java/br/com/dicasdeumdev/demo/services/UserService.java
+++ src/main/java/br/com/dicasdeumdev/demo/services/UserService.java
@@ -2,7 +2,10 @@ package br.com.dicasdeumdev.demo.services;
 
 import br.com.dicasdeumdev.demo.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 }
